70c8678910667e36396de9dd2bdc13edff0aa1c7
Sakshi Garg <writetosakshi333@gmail.com>
2025-09-14 15:47:31 +0530

    added files

---
+++ script.js
@@ -0,0 +1,46 @@
+alert("Welcome! Your voice matters here - report safely and securly");
+function hide(){
+    let btn=document.getElementById('item1');
+    let para=document.getElementById('pot');
+    if(para.style.display!='none'){
+    para.style.display='none';
+}
+else{
+    para.style.display='block';
+
+}
+}
+function hide1(){
+    let btn=document.getElementById('item2');
+    let para=document.getElementById('pot1');
+    //  para.style.display='none';
+    if(para.style.display==='none'){
+    para.style.display='block';
+}
+else{
+    para.style.display='none';
+
+}
+}
+function hide2(){
+    let btn=document.getElementById('item3');
+    let para=document.getElementById('pot2');
+    if(para.style.display!='none'){
+    para.style.display='none';
+}
+else{
+    para.style.display='block';
+
+}
+}
+function hide3(){
+    let btn=document.getElementById('item4');
+    let para=document.getElementById('pot3');
+    if(para.style.display!='none'){
+    para.style.display='none';
+}
+else{
+    para.style.display='block';
+
+}
+}
